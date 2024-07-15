aef4e369ca9f0c71664761a386b3bfe8e559e0d0
MazaevKA <mazaj.ru@mail.ru>
2024-07-15 23:00:25 +0500

    fixed bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,12 +8,22 @@ export function App() {
 		'div',
 		{ className: 'App' },
 		createElement(
-      'header',
-      { className: 'App-header' },
-      createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-      createElement('p'),
-      createElement('a', {className: 'App-link', href: 'https://reactjs.org', target: "_blank", rel: 'noopener noreferrer'}, 'Learn React'),
-      new Date().toLocaleString()
-    ),
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement('p', {}, 'Edit ', createElement('code', {}, 'src/App.js '), 'and save to reload.'),
+
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			new Date().toLocaleString(),
+		),
 	);
-}
+};
